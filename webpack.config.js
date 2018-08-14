695ad76d80d80cb270ab2d52727e6aad1e142ed5
dariogarciag <dario.garcia@yogome.com>
2018-08-14 12:56:39 -0500

    configuracion webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/js/index.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist/js'),
+    filename: 'main.js',
+  },
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node-modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: '../index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '../css/[name].css',
+    }),
+  ],
+};
