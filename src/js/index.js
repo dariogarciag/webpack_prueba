e3cd2a44106c0a41c7652391ac5a11cf17f3a19d
dariogarciag <dario.garcia@yogome.com>
2018-08-21 08:23:45 -0500

    settings

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
-import '../scss/main.scss';
+import '../scss/main.scss'
 
-console.log('hola');
+console.log('hola')
+
+class Persona {
+  constructor (name, age, genre) {
+    this.name = name
+    this.age = age
+    this.genre = genre
+  }
+
+  saluda () {
+    return `Hola ${this.name} tienes ${this.age} y eres ${this.genre}`
+  }
+}
+
+const dario = new Persona('Dario', 29, 'Hombre')
+
+console.log(dario.saluda())
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,35 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: {
-    index: './src/js/index.js',
-  },
+  entry: './src/js/index.js',
   output: {
-    publicPath: '/',
     path: path.join(__dirname, 'dist/js'),
-    filename: 'main.js',
+    filename: 'main.js'
   },
   devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    host: '0.0.0.0',
-    port: 9000,
-    historyApiFallback: true,
-  },
+  // devServer: {
+  //   contentBase: path.join(__dirname, 'dist'),
+  //   host: '0.0.0.0',
+  //   port: 9000,
+  //   historyApiFallback: true,
+  // },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node-modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env'],
-          },
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.(sa|sc|c)ss$/,
         exclude: /node-modules/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-    ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
   },
   optimization: {
     splitChunks: {
@@ -44,18 +38,18 @@ module.exports = {
           name: 'styles',
           test: /\.css$/,
           chunks: 'all',
-          enforce: true,
-        },
-      },
-    },
+          enforce: true
+        }
+      }
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: '../index.html',
-      template: './src/index.html',
+      template: './src/index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: '../css/[name].css',
-    }),
-  ],
-};
+      filename: '../css/main.css'
+    })
+  ]
+}
